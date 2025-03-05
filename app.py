bec56f62da7bcb8e44a2396a07894038397c3a6a
Chiranjibi Lawati <chiranjibilawati08@gmail.com>
2025-03-05 18:58:51 +0400

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-  return "Hello Lawati!"
+def hello_lawati():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
